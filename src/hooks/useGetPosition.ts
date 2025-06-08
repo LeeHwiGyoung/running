66f01b0d2cf92bce4e10c821472db020cd90b08e
HwiGyoung Lee <ter0000@naver.com>
2025-06-08 18:55:00 +0900

    ✨ useGetPosition : geolocation 을 이용하여 현재 위치를 실시간으로 감지하는 기능 hooks 추가

---
+++ src/hooks/useGetPosition.ts
@@ -0,0 +1,40 @@
+'use client';
+import { UserPosition } from '../types/type';
+import { useEffect, useRef, useState } from "react";
+
+
+export default function useGetPosition() {
+  const [position , setPosition] = useState<UserPosition>({
+    longitude : null,
+    latitude : null
+  });
+  const [error , setError] = useState<string|null>(null);
+  const watchId = useRef<number|null>(null);
+
+  useEffect(()=> {
+    if(navigator.geolocation){
+        watchId.current = navigator.geolocation.watchPosition(
+            (pos)=> {
+            setPosition({
+                longitude: pos.coords.longitude,
+                latitude: pos.coords.latitude,
+            });
+            setError(null)
+        },
+        (error) => {
+            setError(error.message);
+        }, {
+            enableHighAccuracy : true,
+            maximumAge : 0
+        })
+    }else {
+    setError('Geolocation을 지원하지 않는 브라우저입니다.')
+    } 
+    return () => {
+        if(watchId.current) {
+            navigator.geolocation.clearWatch(watchId.current);
+        }
+    }
+  }, [])
+  return { position , error}
+}
