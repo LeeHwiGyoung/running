ab2693e0e5713b3b1afe0e2a6a256187f2b6c037
HwiGyoung Lee <ter0000@naver.com>
2025-06-08 18:53:10 +0900

    ✨ KakaoMap 추가 : 현재 위치를 기반으로 카카오 맵을 랜더링 하는 컴포넌트 추가

---
+++ src/component/running/KakaoMap.tsx
@@ -0,0 +1,52 @@
+'use client';
+import useGetPosition from '@/hooks/useGetPosition';
+import Script from 'next/script';
+import React, {useEffect, useRef } from 'react';
+//import markerImage from '@/assets/currentMarker.svg'
+export default function KaKaoMap() {
+  const {position , error} = useGetPosition();
+  const mapMarker = useRef(null);
+  const map = useRef(null);
+  const mapRef = useRef<HTMLDivElement>(null);
+     
+
+  const handleMapLoad = () => {
+     if(!window.kakao.maps || !mapRef.current) return;
+     window.kakao.maps.load(()=> {
+        const options = {
+            center : new window.kakao.maps.LatLng(position.latitude, position.longitude),
+            level : 3,
+            draggable  : false,
+            scrollwheel : false,
+            disableDoubleClick : true,
+            disableDoubleClickZoom  : true,
+        }
+        map.current = new window.kakao.maps.Map(mapRef.current, options)
+     })
+  }
+  
+  useEffect(()=> {
+    if(map.current){
+      const newPos = new window.kakao.maps.LatLng(position.latitude, position.longitude)
+      map.current.setCenter(newPos)
+      mapMarker.current = new window.kakao.maps.Marker({position  : newPos });
+      mapMarker.current.setMap(map.current)
+    }
+    return ()=> {
+      if(map.current && mapMarker.current){
+        mapMarker.current.setMap(null);
+      }
+    }
+  }, [position])
+
+  return (
+    <>
+      <Script
+          src={process.env.NEXT_PUBLIC_KAKAO_MAP_API}
+          strategy='afterInteractive'
+          onLoad={()=> handleMapLoad()}
+      />
+      <div ref={mapRef} id="map" className='w-full h-[80vh] mask-radial-circle mask-radial-from-[10%]'/>
+    </>
+  )
+}
