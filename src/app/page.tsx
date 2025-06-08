caa885bfa1baa5cf88e84307671c4ab9486b1ad9
HwiGyoung Lee <ter0000@naver.com>
2025-06-08 18:54:09 +0900

    ✨ 러닝 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import KaKaoMap from "@/component/running/KakaoMap";
+import RunningGoalInput from "@/component/running/RunningGoalInput";
+import RunningMenu from "@/component/running/RunningMenu";
+
 export default function Home() {
+
   return (
    <main>
-    
-   </main>
+      <h2>러닝</h2>
+      <RunningGoalInput/>
+      <KaKaoMap/>
+      <RunningMenu/>
+    </main>
   );
 }
