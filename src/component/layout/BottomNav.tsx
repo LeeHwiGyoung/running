04db58d6929d8db72d3d1209d4a08144d1773d00
HwiGyoung Lee <ter0000@naver.com>
2025-06-09 15:39:41 +0900

    ✨ BottomNav , BottomNavItem : Img 추가

--- src/component/layout/BottomNav.tsx
+++ src/component/layout/BottomNav.tsx
@@ -3,18 +3,18 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import BottomNavItem from '../ui/BottomNavItem';
-
 export default function BottomNav() {
   const pathname = usePathname();
 
   const navItems = [
-    {path : '/' , label : '러닝', isActive : pathname.startsWith('/')},
-    {path : '/maraton' , label : "마라톤 정보" , isActive : pathname.startsWith('/maraton')},
-    {path : '/dashboard' , label : "내 기록" , isActive : pathname.startsWith('/dashboard')}
+    {path : '/' , label : '러닝', isActive : pathname.startsWith('/'), imageSrc : '/running.svg'},
+    {path : '/maraton' , label : "마라톤 정보" , isActive : pathname.startsWith('/maraton'), imageSrc : '/maraton.svg'},
+    {path : '/dashboard' , label : "내 기록" , isActive : pathname.startsWith('/dashboard'), imageSrc : '/dashboard.svg'}
   ]
+
   return (
-    <nav className='fixed flex bottom-0 w-full border-t'>
-      {navItems.map((navItem)=> <BottomNavItem key={navItem.path} href={navItem.path} label={navItem.label} isActive={navItem.isActive}/>)}
+    <nav className='fixed flex w-full left-0 bottom-0 border-t z-100 bg-white'>
+      {navItems.map((navItem)=> <BottomNavItem key={navItem.path} href={navItem.path} label={navItem.label} isActive={navItem.isActive} imageSrc={navItem.imageSrc}/>)}
     </nav>
   )
 }
--- src/component/ui/BottomNavItem.tsx
+++ src/component/ui/BottomNavItem.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,12 +6,14 @@ interface BottomNavItemProps {
     href : string;
     label : string;
     isActive : boolean;
+    imageSrc : string;
 }
 
-export default function BottomNavItem({href , label } : BottomNavItemProps) {
+export default function BottomNavItem({href , label, imageSrc } : BottomNavItemProps) {
   
   return (
-    <Link className='grow-1 border border-collapse' href={href}>
+    <Link className='flex flex-col grow-1 items-center justify-center ' href={href}>
+        <Image src={imageSrc} alt="" width={24} height={24}/>
         <span>{label}</span>
     </Link>
   )
