131b60142250d44e91a2fc5c0bfe66ea4a05f0bb
HwiGyoung Lee <ter0000@naver.com>
2025-06-09 15:41:03 +0900

    💄 page.tsx : 스타일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import RunningMenu from "@/component/running/RunningMenu";
 export default function Home() {
 
   return (
-   <main>
-      <h2>러닝</h2>
-      <RunningGoalInput/>
-      <KaKaoMap/>
-      <RunningMenu/>
-    </main>
+    <>
+     <header className="py-4">
+        <h2 className="font-bold text-lg p-2">러닝</h2>
+     </header>
+     <main className="relative w-full h-[calc(100dvh-7.7rem)] overflow-hidden">
+        <RunningGoalInput className="absolute z-100 top-20 right-[50%] translate-[50%]"/>
+        <RunningMenu className="absolute z-100 bottom-20 right-[50%] translate-[50%]"/>
+        <KaKaoMap className="absolute inset-0 mask-[radial-gradient(circle,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_75%)]"/>
+      </main>
+    </>
   );
 }
