6bd021ba999a05a25c92f95d867e7425ec6ffc19
HwiGyoung Lee <ter0000@naver.com>
2025-06-12 17:18:56 +0900

    ✨page.tsx(dashboard/runningDetail/id) : 대시보드에서 러닝 카드를 클릭했을 때 상세한 러닝 정보를 제공하는 페이지 추가

---
+++ src/app/dashboard/runningDetail/[id]/page.tsx
@@ -0,0 +1,32 @@
+import DetailRunCard from '@/component/dashboard/DetailRunCard'
+import BackButton from '@/component/ui/BackButton'
+import React from 'react'
+
+const dummyDetail = {
+    id: 0,
+    date: '2025-06-09',
+    title : "월요일 저녁 러닝",
+    distance : 3,
+    distanceLabel : 'Km',
+    runningPace : 450,
+    runningTime : 1200,
+    cadence : 180,
+    altitude : 2,
+}
+
+export default function page() {  
+  return (
+    <main>
+        <BackButton />
+        <DetailRunCard 
+         date={dummyDetail.date}
+         title={dummyDetail.title}
+         distance={dummyDetail.distance}
+         distanceLabel={dummyDetail.distanceLabel}
+         runningPace={dummyDetail.runningPace}
+         runningTime={dummyDetail.runningTime}
+         cadence={dummyDetail.cadence} 
+         />
+    </main>
+  )
+}
