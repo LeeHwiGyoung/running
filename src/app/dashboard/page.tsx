ed78686480e9e38506106ae20a86ba64111359ab
HwiGyoung Lee <ter0000@naver.com>
2025-06-12 15:44:09 +0900

    🎨 page.tsx(dashboard) : RunCard 컴포넌트 리스트를 RunCardList 로 변경

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,41 +1,7 @@
-import RunCard from '@/component/dashboard/RunCard'
+import RunCardList from '@/component/dashboard/RunCardList'
 import Total from '@/component/dashboard/Total'
 import React from 'react'
 
-const RunCardDummyData = 
-    [ 
-        {
-            date: '2025-06-09',
-            title : "월요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200,
-        }, 
-        {
-            date: '2025-06-08',
-            title : "일요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200, 
-        },
-        {
-            date: '2025-06-07',
-            title : "토요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200, 
-        },
-        {
-            date: '2025-06-06',
-            title : "금요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200, 
-        }] 
 
 export default function page() {
   return (
@@ -49,13 +15,7 @@ export default function page() {
         </section>
         <section className='space-y-4'>
             <h3>최근 활동</h3>
-            <ul className='flex flex-col gap-8 px-2'>            
-                {RunCardDummyData.map((runData)=> 
-                    <li key={runData.date + runData.title}>
-                        <RunCard date={runData.date} title={runData.title} distance={runData.distance} distanceLabel={runData.distanceLabel} runningPace={runData.runningPace} runningTime={runData.runningTime}/>
-                    </li>
-                )}                
-            </ul>
+            <RunCardList /> 
         </section>
       </main>
     </>
