0b43f78359efd4dbd0daea0e87f12e83da59bad1
HwiGyoung Lee <ter0000@naver.com>
2025-06-18 15:30:35 +0900

    🎨 LineGraph : y축의 포맷함수 props 와 reverseyAxis 추가

--- src/component/dashboard/LineGraph.tsx
+++ src/component/dashboard/LineGraph.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3';
-import { formatPace, formatTime } from '@/utils/format';
+import {  formatTime } from '@/utils/format';
 
 interface Geometry {
   type: string;
@@ -28,6 +28,7 @@ interface PaceFeature {
 }
 
 interface LineGraphProps {
+    id: string;
     data :  PaceFeature[];
     xKey : string;
     yKey : string;
@@ -37,41 +38,49 @@ interface LineGraphProps {
     marginBottom ?: number;
     marginRight ?: number;
     marginLeft ?: number;
-    setAvg : React.Dispatch<React.SetStateAction<string>>;
+    reverseYAxis ?: boolean;
+    tickFormatFunc ?: (value :number|string) => string;
+    setButtonText : React.Dispatch<React.SetStateAction<string>>;
     setHoveredSegmentId : React.Dispatch<React.SetStateAction<number|null>>
 }
-export default function LineGraph({data , xKey , yKey , width =400 ,height=200, marginBottom= 20, marginLeft=20, marginRight=20 , marginTop= 20 , setAvg , setHoveredSegmentId}:LineGraphProps) {
+export default function LineGraph({data , id , xKey , yKey ,reverseYAxis= false ,  width =400 ,height=200, marginBottom= 20, marginLeft=20, marginRight=20 , marginTop= 20 , setButtonText , setHoveredSegmentId, tickFormatFunc}:LineGraphProps) {
   const svgRef = useRef<SVGSVGElement | null>(null);
+  
   useEffect(()=> {
     const processedData = data.slice(1); 
     const svg = d3
     .select(svgRef.current)
     .attr('width' , width) //width 설정
     .attr('height', height) //height 설정
-    .attr('viewBox', [0,0,width, height])
+    .attr('viewBox', [0,0, width, height])
     .attr('style', 'max-width : 100%; height : auto')
 
     svg.selectAll('*').remove(); //svg 초기화
 
-    const xMaxDistance = d3.max(processedData, d => d.properties[xKey]) || 0;
+    const xMax = d3.max(processedData, d => d.properties[xKey]) || 0;
 
     const yMin = d3.min(processedData, d => d.properties[yKey]) || 0;
     const yMax = d3.max(processedData, d => d.properties[yKey]) || 0;
 
     const x = d3
     .scaleLinear()
-    .domain([0, xMaxDistance])
+    .domain([0, xMax])
     .range([marginLeft , width - marginRight])
     
     const y = d3
     .scaleLinear()
-    .domain([yMax , yMin])
-    .range([height - marginBottom , marginTop])
+    
+    if(reverseYAxis){
+      y.domain([yMax,yMin])
+    }else{
+      y.domain([yMin,yMax])
+    }
+    y.range([height - marginBottom , marginTop])
 
 
     const line = d3.line<PaceFeature>()
-      .x(d => x(d.properties.cumulativeDistanceKm)) // X 좌표: 누적 거리
-      .y(d => y(d.properties.averagePaceSecondsPerKm)) // Y 좌표: 평균 페이스
+      .x(d => x(d.properties[xKey])) // X 좌표
+      .y(d => y(d.properties[yKey])) // Y 좌표
       .curve(d3.curveBasis);
 
     svg
@@ -84,9 +93,17 @@ export default function LineGraph({data , xKey , yKey , width =400 ,height=200, 
     .style("-ms-user-select", "none")
     .style("user-select", "none")
 
-    svg.append("g")
-      .attr('transform', `translate(${marginLeft} , 0)`)
-      .call(d3.axisLeft(y).ticks(5).tickFormat(d => formatPace(d as number)))
+    
+    const yAxisSvg = svg.append("g")
+      .attr('transform', `translate(${marginLeft} , 0)`);
+    
+      if(tickFormatFunc) {
+        yAxisSvg.call(d3.axisLeft(y).ticks(5).tickFormat(d => tickFormatFunc(d as number)));
+      }else{
+        yAxisSvg.call(d3.axisLeft(y).ticks(5))
+      }
+      
+      yAxisSvg
       .style("-webkit-user-select", "none")
       .style("-moz-user-select", "none")
       .style("-ms-user-select", "none")
@@ -129,7 +146,7 @@ export default function LineGraph({data , xKey , yKey , width =400 ,height=200, 
     .on("mouseover", () => { focus.style("display", null); }) // 마우스 오버 시 툴팁 보이기
     .on("mouseout", () => {  
        focus.style("display", "none");
-       setAvg(null);
+       setButtonText(null);
        setHoveredSegmentId(null); }) // 마우스 아웃 시 툴팁 숨기기
     .on("mousemove", mousemove)
 
@@ -146,23 +163,27 @@ export default function LineGraph({data , xKey , yKey , width =400 ,height=200, 
       } else if (!d0) { // d0가 undefined (즉, x0가 첫 데이터보다 작을 때, i=0인 경우)
         d = d1; // d1이 첫 데이터이므로 d1을 선택 (사실 processedData.slice(1)을 쓰므로 이 경우는 발생 가능성 낮음)
       } else { // d0와 d1이 모두 유효할 때, 더 가까운 데이터 선택
-        d = mouseX - d0.properties.cumulativeDistanceKm > d1.properties.cumulativeDistanceKm - mouseX ? d1 : d0;
+        d = mouseX - d0.properties[xKey] > d1.properties[xKey] - mouseX ? d1 : d0;
       }
       
-      const tooltipX = x(d.properties.cumulativeDistanceKm);
+      const tooltipX = x(d.properties[xKey]);
       focus.attr("transform", `translate(${tooltipX})`)
       focus.select('.x-hover-text')
        .attr('x', 0) // focus 그룹 기준 X=0 (즉, 수직선 바로 위)
        .attr('y', 15)
        .text(`${formatTime(d.properties.elapsedTimeSeconds)}`)
 
-       setAvg(d.properties.averagePaceFormatted)
+       if(tickFormatFunc){
+         setButtonText(tickFormatFunc(d.properties[yKey]))
+       }else{
+         setButtonText(d.properties[yKey])
+       }
        setHoveredSegmentId(d.properties.id)
 
     }
   }, [])
 
   return (
-    <svg ref={svgRef}></svg>
+    <svg id={id} ref={svgRef}></svg>
   )
 }
