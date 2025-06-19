07b023f6f83a511fa9b2620d7bd1ae3b868a283d
HwiGyoung Lee <ter0000@naver.com>
2025-06-19 16:09:51 +0900

    🏷️ Coordinate 타입 , CarouselData  타입 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,4 +1,13 @@
-export type UserPosition = {
+export type Coordinate = {
     latitude : number;
     longitude : number;
+}
+
+export type UserPosition = Coordinate & {
+    timestamp : number;
+}
+
+export type CarouselData = {
+    value : number | string | null,
+    label : string;
 }
