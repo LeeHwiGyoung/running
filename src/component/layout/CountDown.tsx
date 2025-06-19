c2b46f19d6aaedb10099f8176ed20b7924dfb53f
HwiGyoung Lee <ter0000@naver.com>
2025-06-19 16:06:55 +0900

    ✨ CountDown : 카운트 다운을 랜더링하며 카운트 다운이 끝나면 tracking 상태를 변경시키는 컴포넌트

---
+++ src/component/layout/CountDown.tsx
@@ -0,0 +1,27 @@
+import { useCountDown } from '@/hooks/useCountDown'
+import { useRunningStore } from '@/store/useRunningStore';
+import React, { useEffect } from 'react'
+
+export default function CountDown() {
+  const { setTracking } = useRunningStore();
+  
+  const {count ,startCountDown , isCountDownRuninng } = useCountDown({initCount : 3});
+
+  useEffect(()=> {
+    startCountDown();
+  }, [])
+
+  useEffect(() => {
+    // 카운트다운이 끝나고 (count가 0이 되고) 더 이상 실행 중이 아닐 때만 tracking을 true로 설정합니다.
+    if (count === 0 && !isCountDownRuninng) {
+      setTracking(true);
+    }
+  }, [count, isCountDownRuninng, setTracking])
+  
+  return (
+    <article className='flex items-center justify-center w-full h-[100vh] bg-black'>
+        <h2 className='sr-only'>카운트 다운</h2>
+        <p key={count} className='text-white text-9xl animate-countdown'>{count}</p>
+    </article>
+  )
+}
