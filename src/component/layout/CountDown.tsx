c5d6c01a0addd609e08df560da4c293fef0e04b8
HwiGyoung Lee <ter0000@naver.com>
2025-06-20 17:32:51 +0900

    ✨ CountDown : 카운트 다운이 종료되면 step을 변경 시키는 기능 추가

--- src/component/layout/CountDown.tsx
+++ src/component/layout/CountDown.tsx
@@ -3,8 +3,7 @@ import { useRunningStore } from '@/store/useRunningStore';
 import React, { useEffect } from 'react'
 
 export default function CountDown() {
-  const { setTracking } = useRunningStore();
-  
+  const { setTracking , setStep} = useRunningStore();
   const {count ,startCountDown , isCountDownRuninng } = useCountDown({initCount : 3});
 
   useEffect(()=> {
@@ -15,8 +14,9 @@ export default function CountDown() {
     // 카운트다운이 끝나고 (count가 0이 되고) 더 이상 실행 중이 아닐 때만 tracking을 true로 설정합니다.
     if (count === 0 && !isCountDownRuninng) {
       setTracking(true);
+      setStep(1);
     }
-  }, [count, isCountDownRuninng, setTracking])
+  }, [count, isCountDownRuninng, setTracking, setStep])
   
   return (
     <article className='flex items-center justify-center w-full h-[100vh] bg-black'>
