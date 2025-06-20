10f93ff6c9804a9ccb1a87b24d5a52935880de91
HwiGyoung Lee <ter0000@naver.com>
2025-06-20 17:27:36 +0900

    🎨 page.tsx(/) : runningGoal 을 input 에서 modal 을 띄우는 버튼으로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import KaKaoMapWithCheckPosition from "@/component/KakaoMapWithCheckPosition";
-import RunningGoalInput from "@/component/running/RunningGoalInput";
+import RunningGoal from "@/component/running/RunningGoal";
 import RunningMenu from "@/component/running/RunningMenu";
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
         <h2 className="font-bold text-lg p-2">러닝</h2>
      </header>
      <main className="relative w-full h-[calc(100dvh-7.7rem)] overflow-hidden">
-        <RunningGoalInput className="absolute z-100 top-20 right-[50%] translate-[50%]"/>
+        <RunningGoal className="flex flex-col font-bold text-2xl absolute z-100 top-20 right-[50%] translate-[50%] w-[50%] max-w-[20rem]"/>
         <RunningMenu className="absolute z-100 bottom-20 right-[50%] translate-[50%]"/>
         <KaKaoMapWithCheckPosition className="absolute inset-0 mask-[radial-gradient(circle,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_75%)]"/> 
       </main>
