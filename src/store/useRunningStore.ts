db510b7b7e53f4fb9b72fe93cbc2acbdddd6bb0f
HwiGyoung Lee <ter0000@naver.com>
2025-06-20 17:33:09 +0900

    ✨ useRunningStore : step 과 setStep 을 전역으로 관리

--- src/store/useRunningStore.ts
+++ src/store/useRunningStore.ts
@@ -5,12 +5,15 @@ interface RunningState {
     isTracking : boolean;
     setRunningGoal : (value : number | null) => void;
     setTracking : (value : boolean) => void;
+    step : 0 | 1,
+    setStep : (value : 0 | 1) => void;
 }
 
 export const useRunningStore = create<RunningState>((set) => ({
     runningGoal : 0,
     isTracking : false,
+    step : 0,
     setRunningGoal: (value) => set({runningGoal : value }),
-    setTracking : (value) => set({isTracking : value})
-
+    setTracking : (value) => set({isTracking : value}),
+    setStep: (value)=> set({step: value}) 
 }))
