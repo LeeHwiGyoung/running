fc138b15534bef995035ffe422ee20fc3f893b54
HwiGyoung Lee <ter0000@naver.com>
2025-06-20 17:27:08 +0900

    🔥 KakaoMap : polyLine 관련 로직 제거 ( 무한 리랜더링을 야기하여 로직 변경 후 추가 예정)

--- src/component/KakaoMap.tsx
+++ src/component/KakaoMap.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Script from 'next/script';
-import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from 'react';
 
 
 export interface KakaoMapHandle 
@@ -25,10 +25,10 @@ interface KakaoMapProps {
   hoveredSegmentId ?: number|null;
 }
 
-const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className,mapId , level , latitude , longitude , dummyPath, hoveredSegmentId}, ref) => {
+const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className,mapId , level , latitude , longitude }, ref) => {
   const map = useRef(null);
   const mapRef = useRef<HTMLDivElement>(null);
-  const [statePolyline,setStatePolyline] = useState([]);     
+     
   useImperativeHandle(ref, () => ({
     getMap: () => map.current, // 외부에서 지도를 얻을 수 있는 메서드 제공
     getKaKaoInstance : () => window.kakao,
@@ -46,45 +46,16 @@ const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className,mapId , le
             disableDoubleClickZoom  : true,
         }
         map.current = new window.kakao.maps.Map(mapRef.current, options)
-        if(dummyPath && dummyPath.length > 1){
-          // 기존 폴리라인들 제거
-          statePolyline.forEach(({polyline}) => {
-            polyline.setMap(null);
-          });
-          setStatePolyline([])
-
-          // 각 세그먼트별로 폴리라인 생성
-          for(let i = 0; i < dummyPath.length - 1; i++) {
-            const startPoint = dummyPath[i];
-            const endPoint = dummyPath[i + 1];
-            
-            const polyline = new window.kakao.maps.Polyline({
-              map : map.current,
-              path : [
-                new window.kakao.maps.LatLng(startPoint.latitude, startPoint.longitude),
-                new window.kakao.maps.LatLng(endPoint.latitude, endPoint.longitude)
-              ],
-              strokeWeight: 2,
-              strokeColor: '#FF00FF',
-              strokeOpacity: 0.8,
-              strokeStyle: 'solid'
-            });
-            
-            polyline.setMap(map.current);
-            
-            // 폴리라인과 세그먼트 ID를 함께 저장
-            setStatePolyline(prevPolylines => [...prevPolylines, {polyline : polyline , segmentId : endPoint.id}]);
-          }
-        }
+        
+        
      })
-  }, [dummyPath, latitude, level, longitude])
+  }, [latitude, level, longitude])
 
 
-   useEffect(() => {
+   /* useEffect(() => {
     if (hoveredSegmentId && statePolyline) {
     statePolyline.forEach(({polyline, segmentId }) => {
       if (segmentId === hoveredSegmentId) { // 조건이 더 단순해졌습니다.
-        console.log(polyline ,'polyline')
         polyline.setOptions({
           strokeColor : '#000000'
         });
@@ -100,11 +71,11 @@ const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className,mapId , le
     });
   }
   }, [hoveredSegmentId, statePolyline]);
-
+ */
    useEffect(() => {
     // 이미 kakao API가 로드되어 있는 경우 바로 초기화
     if (window.kakao?.maps) {
-      initializeMap();
+       initializeMap();
     }
   }, [initializeMap]);
   return (
