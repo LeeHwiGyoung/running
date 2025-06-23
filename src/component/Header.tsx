2bd3b0375fb29f8feca74b50ca3c6d0b9e0e9039
HwiGyoung Lee <ter0000@naver.com>
2025-06-23 16:44:33 +0900

    ✨ Header  : 헤더의 공통 스타일링을 위한 헤더컴포넌트 추가

---
+++ src/component/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface HeaderProps {
+    className ?: string;
+    children ?: React.ReactNode;
+}
+export default function Header(
+    {children , className} : HeaderProps
+) {
+  return (
+    <header className={`p-4 font-bold text-lg ${className}`}>
+        {children}
+    </header>
+  )
+}
