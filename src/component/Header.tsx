b6e466c4cf39345f0ceebcef8372111a7b273ddf
HwiGyoung Lee <ter0000@naver.com>
2025-06-23 17:50:50 +0900

    ✨ Header.tsx : borderBottomShadow props 추가

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 interface HeaderProps {
     className ?: string;
     children ?: React.ReactNode;
+    borderBottomShadow ?: boolean;
 }
 export default function Header(
-    {children , className=""} : HeaderProps
+    {children , className="" , borderBottomShadow = false} : HeaderProps
 ) {
   return (
-    <header className={`p-4 font-bold text-lg ${className}`}>
+    <header className={`p-4 font-bold text-lg ${borderBottomShadow && `shadow-[0px_10px_10px_-10px_rgba(33,35,38,0.1)]`} ${className}`}>
         {children}
     </header>
   )
