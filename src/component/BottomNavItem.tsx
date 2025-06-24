67b6063663f4ddcca81ab4ff85e22fb0c07f0d67
HwiGyoung Lee <ter0000@naver.com>
2025-06-24 16:08:37 +0900

    ✨ BottomNavItem : activeColor props 를 추가하여 isActive가 true 이면 색상이 변경되는 기능 추가

--- src/component/BottomNavItem.tsx
+++ src/component/BottomNavItem.tsx
@@ -9,9 +9,10 @@ interface BottomNavItemProps {
     label : string;
     isActive : boolean;
     imageSrc : string;
+    activeColor : string;
 }
 
-export default function BottomNavItem({href , label, imageSrc } : BottomNavItemProps) {
+export default function BottomNavItem({href , label, imageSrc , isActive , activeColor } : BottomNavItemProps) {
   const router = useRouter();
   const {isTracking} = useRunningStore();
 
@@ -23,8 +24,9 @@ export default function BottomNavItem({href , label, imageSrc } : BottomNavItemP
       router.push(href);
     }
   };
+
   return (
-    <Button className='flex flex-col grow-1 items-center justify-center ' onClick={handleClick}>
+    <Button className={`flex flex-col grow-1 items-center justify-center ${isActive && activeColor}`} onClick={handleClick}>
         <Image src={imageSrc} alt="" width={24} height={24}/>
         <span>{label}</span>
     </Button>
