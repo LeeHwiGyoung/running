d4944e1207fb2c269b8cdcdad46523294013b5e6
HwiGyoung Lee <ter0000@naver.com>
2025-06-24 17:30:28 +0900

    🎨 DetailRunCard: 타이틀 편집 기능 추가 - isEditable: 타이틀 편집 가능 여부를 제어하는 props - onTitleChange: 수정된 타이틀을 부모 컴포넌트로 전달하는 콜백 함수 props - isEditing: 타이틀 편집 모드 상태를 관리하는 state - editTitle: 편집 중인 타이틀 값을 저장하는 local state - titleRef: 편집 모드 진입 시 input에 자동 포커스를 위한 ref

--- src/component/dashboard/DetailRunCard.tsx
+++ src/component/dashboard/DetailRunCard.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { formatPace, formatTime } from '@/utils/format'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import useCountUp from '@/hooks/useCountUp'
+import Button from '../Button';
+import Image from 'next/image';
 
 interface DetailRunCardProps {
     date: string,
@@ -11,6 +13,8 @@ interface DetailRunCardProps {
     runningPace : number,
     runningTime : number,
     cadence : number,
+    isEditable ?: boolean,
+    handleChangeTitle ?: (newTitle : string) => void;
 }
 
 export default function DetailRunCard({
@@ -21,17 +25,66 @@ export default function DetailRunCard({
     runningPace,
     runningTime,
     cadence,
+    isEditable = false,
+    handleChangeTitle,
 }:DetailRunCardProps) {
   const count = useCountUp({target : distance})
-  
+  const [isEdit , setIsEdit] = useState<boolean>(false);
+  const [editTitle, setEditTitle] = useState<string>(title);
+  const titleRef = useRef<HTMLInputElement|null>(null); 
+
+  const handleEditTitleBtn = () => {
+    setIsEdit(true);
+  }
+
+  const handleCheckTitleBtn = () => {
+    if(!editTitle) { 
+     alert('제목을 입력해주세요');
+     return   
+    }
+    setIsEdit(false);
+    handleChangeTitle((editTitle))
+  }
+
+  const handleEditTile = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setEditTitle(event.target.value);
+  }
+
+  useEffect(()=> {
+    if(isEdit === true) {
+        titleRef.current.focus();
+    }
+  }, [isEdit])
+
   return (
-    <article className='flex flex-col gap-4 px-4'>
-        <div>
-            <h2 className='text-xl font-bold'>
+    <article className='flex flex-col gap-2 px-4'>
+        {
+            (isEditable  && isEdit) ? 
+            <div className='flex border-b-2 border-gray-200'>
+                <label className='sr-only' htmlFor="title">러닝 제목</label>
+                <input
+                ref={titleRef}
+                id="title"
+                name='title'
+                className='font-bold text-xl py-2 grow-1'
+                type='text'
+                onChange={handleEditTile}
+                value={editTitle} />
+                <Button className='ml-auto' onClick={handleCheckTitleBtn}>
+                    <Image src={'/check.svg'} alt={'제목 수정 완료'} width={24} height={24}/>
+                </Button>
+                </div>
+            :
+            <h2 className='flex text-xl font-bold border-gray-200 border-b-2 py-2'>
                 {title}
+                {isEditable && (
+                    <Button className='ml-auto' onClick={handleEditTitleBtn}>
+                        <Image src={'/edit.svg'} alt={'제목 수정'} width={24} height={24}/>
+                    </Button>
+                )}
             </h2>
-            <time className='text-gray-400' dateTime={date}>{date}</time> 
-        </div>
+        }
+        <time className='text-gray-400' dateTime={date}>{date}</time> 
         <div className='flex items-end text-5xl gap-1'>
             <data className="font-bold" value={distance}>{count.toFixed(2).toLocaleString()}</data>
             <span className='text-2xl'>{distanceLabel}</span>
