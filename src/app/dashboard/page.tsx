fd2687f38d00a134084b41026522446b7a693152
HwiGyoung Lee <ter0000@naver.com>
2025-06-24 16:06:59 +0900

    ✨ dashboard.tsx : Header 추가

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,12 +11,12 @@ export default function page() {
         통계
       </Header>
       <main className='mb-16'>
+        <Total/>
         <section>
-            <Total/>
-        </section>
-        <section className='space-y-4'>
+          <Header>
             <h3>최근 활동</h3>
-            <RunCardList /> 
+          </Header>
+          <RunCardList /> 
         </section>
       </main>
     </>
