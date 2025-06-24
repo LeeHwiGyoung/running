e8cfe290138e2fd9d1ec4fe3dfffc6b6b5f78521
HwiGyoung Lee <ter0000@naver.com>
2025-06-24 16:07:53 +0900

    🎨 BottomNav.tsx : '/' 경로의 isActive를 일치 연산자를 사용하여 비교하게 변경

--- src/component/BottomNav.tsx
+++ src/component/BottomNav.tsx
@@ -7,13 +7,13 @@ export default function BottomNav() {
   const pathname = usePathname();
 
   const navItems = [
-    {path : '/' , label : '러닝', isActive : pathname.startsWith('/'), imageSrc : '/running.svg'},
+    {path : '/' , label : '러닝', isActive : pathname === '/', imageSrc : '/running.svg'},
     {path : '/dashboard' , label : "내 기록" , isActive : pathname.startsWith('/dashboard'), imageSrc : '/dashboard.svg'}
   ]
 
   return (
-    <nav className='fixed flex w-full left-0 bottom-0 border-t z-1000 bg-white'>
-      {navItems.map((navItem)=> <BottomNavItem key={navItem.path} href={navItem.path} label={navItem.label} isActive={navItem.isActive} imageSrc={navItem.imageSrc}/>)}
+    <nav className='fixed flex w-full left-0 bottom-0 border-t border-gray-300 z-1000 bg-white shadow-[0px_2px_2px_0px_rgba(0,0,0,0.5)]'>
+      {navItems.map((navItem)=> <BottomNavItem key={navItem.path} href={navItem.path} label={navItem.label} isActive={navItem.isActive} imageSrc={navItem.imageSrc} activeColor='bg-stone-100'/>)}
     </nav>
   )
 }
