7638054cc4aa19646f4dd74145983b20d65146e3
HwiGyoung Lee <ter0000@naver.com>
2025-06-30 15:36:04 +0900

    ✨ Header.tsx : ref를 props로 사용하기 위해 forwardRef 추가

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 interface HeaderProps {
     className ?: string;
     children ?: React.ReactNode;
     borderBottomShadow ?: boolean;
 }
-export default function Header(
-    {children , className="" , borderBottomShadow = false} : HeaderProps
-) {
+const Header = forwardRef<HTMLDivElement, HeaderProps>(({ children, className = "" , borderBottomShadow = false}, ref) => {
   return (
-    <header className={`p-4 font-bold text-lg ${borderBottomShadow && `shadow-[0px_10px_10px_-10px_rgba(33,35,38,0.1)]`} ${className}`}>
+    <header ref={ref} className={`p-4 font-bold text-lg ${borderBottomShadow && `shadow-[0px_10px_10px_-10px_rgba(33,35,38,0.1)]`} ${className}`}>
         {children}
     </header>
   )
-}
+});
+
+Header.displayName = 'Header';
+export default Header;
