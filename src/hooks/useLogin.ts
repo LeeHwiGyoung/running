e9149eddd09912ea1e37f0e2de557ce35c39ba35
HwiGyoung Lee <ter0000@naver.com>
2025-06-30 14:25:21 +0900

    ✨ useLogin : login의 로직을 관리하는 hooks 으로 firebase client SDK 를 사용하여 로그인을 하며 백엔드에 idToken을 보내 유효한 경우 세션 쿠키 설정

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,52 @@
+'use client';
+
+import { useAuthStore } from "@/store/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { auth } from "../../lib/firebase/client";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export const useLogin = () => {
+    const [isLoading , setIsLoading] = useState<boolean>(false);
+    const [error ,setError] = useState<string | null> (null);
+
+    const setUser = useAuthStore(state => state.setUser);;
+    const router = useRouter();
+
+    const clientAuth = auth;
+
+    const login = async({email , password}) => {
+        setIsLoading(true);
+        try{
+            const userCredential =  await signInWithEmailAndPassword(clientAuth , email , password);
+            const user = userCredential.user;
+            const userIdToken = await user.getIdToken();
+
+            const response = await fetch('api/user/login', {
+                method : 'POST',
+                headers : {
+                    'Authorization': `Bearer ${userIdToken}`
+                },
+            })
+            if(response.ok){
+                const result = await response.json();
+                setUser({email , nickname : result.name})
+                //router.push('/')
+                console.log('로그인 성공:', result.message);
+            }else {
+                const errorData = await response.json(); // 서버에서 보낸 에러 메시지를 파싱
+                console.error('세션 생성 실패:', errorData.message);
+            }
+        }catch(error){
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setError(`errorCode : ${errorCode}: ${errorMessage}`);
+        }finally{
+            setIsLoading(false);
+        }
+
+    }
+
+    return {login , isLoading , error}
+
+}
