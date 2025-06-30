cfd588b756764573c133f102eec6f7562e4d129a
HwiGyoung Lee <ter0000@naver.com>
2025-06-30 15:34:15 +0900

    ✨ HamburgerButton.tsx : toggleMenuDisplay 를 props로 받는 햄버거 버튼 컴포넌트

---
+++ src/component/HamburgerButton.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
+
+
+interface HamburgerButtonProps {
+    className ?: string;
+    toggleMenuDisplay : () => void;
+}
+export default function HamburgerButton({className= "" , toggleMenuDisplay} : HamburgerButtonProps) {
+  return (
+    <Button type='button' className={`${className}`} onClick={toggleMenuDisplay}>
+        <Image src='/menu.svg' alt="메뉴" width={24} height={24} />
+    </Button>
+  )
+}
