e5843cb804211c99ed1110f53c85bf59d130087f
HwiGyoung Lee <ter0000@naver.com>
2025-07-01 18:21:57 +0900

    🎨 api/auth/logout/route.ts :  중복 코드 제거 - 인증 로직 미들웨어를 사용하여 중복코드 제거

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,17 +1,14 @@
-import { cookies } from "next/headers";
+import { authenticate } from './../../../../../lib/firebase/auth-middleware';
 import { NextRequest, NextResponse } from "next/server";
 import { authAdmin } from "../../../../../lib/firebase/admin";
+import { ErrorType } from '@/types/api.types';
+import { cookies } from 'next/headers';
 
 export async function POST(request : NextRequest) {
     let response;
     try {
-        const body =  await request.json();
-        const logoutType = body.logoutType;
-    
-        const cookie = await cookies();
-        const idToken = cookie?.get('session').value;
-    
-        const verifyIdToken = await authAdmin.verifySessionCookie(idToken, true);
+        const {logoutType} =  await request.json();    
+        const verifyIdToken = await authenticate();
 
         //리프레쉬 토큰 무효화
         if(logoutType === 'all-device' ) {
@@ -21,16 +18,16 @@ export async function POST(request : NextRequest) {
         response = NextResponse.json({message : '로그아웃이 성공적으로 되었습니다.' , status : 200})
        
     }catch(error){
-        console.error(error);
-        response =  NextResponse.json({ message : `로그아웃이 되었습니다.`, status : 200})
+        const err = error as ErrorType;
+        if(err.cause === 'AuthenticationError') { //이미 토큰이 만료되어서
+            response = NextResponse.json({ message : `로그아웃이 되었습니다.`, status : 200})      
+        }else{
+            response = NextResponse.json({message : 'server Errror' , status : 500});
+        }
+        
     }finally {
-        (await cookies()).set('session' , '' , {
-            maxAge : 0,
-            expires : new Date(0),
-            httpOnly : true,
-            secure : process.env.NODE_ENV === 'production',
-            path : '/' 
-        });    
+        (await cookies()).delete('session');
+        //세션 쿠키 무효화
         return  response;
     }
 }
