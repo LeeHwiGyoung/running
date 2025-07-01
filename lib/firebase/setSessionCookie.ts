090f406871c6ea80fe11f0e7657d86475bf65f0a
HwiGyoung Lee <ter0000@naver.com>
2025-07-01 18:17:03 +0900

    ✨ setSessionCookie : 서버 세션 쿠키 설정 헬퍼 함수 (setSessionCookie) 구현

---
+++ lib/firebase/setSessionCookie.ts
@@ -0,0 +1,15 @@
+import { cookies } from "next/headers";
+import { authAdmin } from "./admin";
+
+export async function setSessionCookie(idToken: string, expiresIn: number) {
+    const sessionCookie = await authAdmin.createSessionCookie(idToken, { expiresIn });
+    const cookieStore = await cookies();
+    // Next.js `cookies()` 함수를 사용하여 쿠키 설정
+     cookieStore.set('session', sessionCookie, {
+        maxAge: expiresIn,
+        httpOnly: true, // JavaScript 접근 불가
+        secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
+        sameSite: 'strict',
+        path: '/', // 모든 경로에서 쿠키 접근 가능
+    });
+}
