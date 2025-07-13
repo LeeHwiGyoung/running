84ab689a0c09505f4f2c9a057873c6e16bab4077
HwiGyoung Lee <ter0000@naver.com>
2025-07-13 17:09:02 +0900

    🎨 AuthHeader : 햄버거 버튼의 로직을 Hamburger 컴포넌트로 분리

--- src/component/user/AuthHeader.tsx
+++ src/component/user/AuthHeader.tsx
@@ -1,63 +1,21 @@
 'use client';
-
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import Header from '../Header'
-import HamburgerButton from '../HamburgerButton'
-import HamburgerMenu from '../HamburgerMenu';
+import Hamburger from '../hamburgers/Hamburger';
 
-
-const loggedInMenuItems = [
-    { id: 'myinfo', text: '내 정보', href: '/profile' },
-    { id: 'logout', text: '로그아웃', href: '/logout' }, // 실제 로그아웃 처리 로직 필요
-];
-
-// 로그아웃 시 메뉴 아이템
-const loggedOutMenuItems = [
-    { id: 'signup', text: '회원가입', href: '/signup' },
-    { id: 'login', text: '로그인', href: '/login' },
-];
 
 interface AuthHeaderProps {
     children ?: React.ReactNode;
     menuPosition ?: "left" | "right";
     className ?: string;
 }
+
 export default function AuthHeader({children , menuPosition = 'right', className = ""} : AuthHeaderProps) {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const headerRef = useRef<HTMLDivElement>(null); // 외부 클릭 감지를 위한 Ref
-
-  const toggleMenu = () => {
-    setIsMenuOpen((prev) => !prev);
-  };
-
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-        // headerRef.current가 존재하고, 클릭된 요소가 headerRef.current 내부에 포함되지 않는다면
-        if (headerRef.current && !headerRef.current.contains(event.target as Node)) {
-            setIsMenuOpen(false);
-        }
-    };
-
-    if (isMenuOpen) {
-        document.addEventListener('mousedown', handleClickOutside);
-    } else {
-        document.removeEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [isMenuOpen]);
 
   return (
-    <Header className={`flex items-center z-10000 ${className}`} ref={headerRef}>
+    <Header className={`flex items-center z-10000 ${className}`}>
         {children}
-        <div className={`relative  ${menuPosition ? 'ml-auto' : 'mr-auto'}`}>
-            <HamburgerButton toggleMenuDisplay={toggleMenu} />
-            {isMenuOpen && 
-            <HamburgerMenu items={loggedOutMenuItems} onMenuItemClick={()=> console.log('클릭')}/>}
-        </div>
+        <Hamburger buttonPosition={menuPosition} />
     </Header>
   )
 }
