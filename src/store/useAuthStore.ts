45c277b4d633b3965d82f1122fba8ad4c4d59ade
HwiGyoung Lee <ter0000@naver.com>
2025-08-04 17:41:08 +0900

    ✨ useAuthStore : 유저의 로그인 정보를 저장하는 store 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -3,18 +3,27 @@ import { User } from '@/types/user.types';
 import { create } from 'zustand';
 interface AuthState {
  user : User;
+ isLoggedIn : boolean;
 }
 
 interface AuthActions {
- setUser : (value : User) => void; 
+ setUser : (value : User) => void;
+ setIsLoggedIn  : (value : boolean) => void;
+ clearUser : () => void;
 }
 
 export const useAuthStore =  create<AuthState & AuthActions>((set) => ({
-    user :{ nickname : "" , email : ""},
+    user :{ nickname : "" , email : "" },
+    isLoggedIn : false,
     setUser : (value) => set({
         user : {
             nickname : value.nickname,
-            email : value.email
+            email : value.email,
         }
+    }),
+    setIsLoggedIn : (value) => set({isLoggedIn : value}),
+    
+    clearUser : () => set({
+        user : {nickname : "" , email : ""}
     })
 }))
