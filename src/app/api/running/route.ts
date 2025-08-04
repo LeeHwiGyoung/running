1a63f557532855f7326771d741864bf4909711fa
HwiGyoung Lee <ter0000@naver.com>
2025-08-04 17:35:58 +0900

    🎨 api/running : transaction 을 통하여 원자적 처리가 가능하게 변경

--- src/app/api/running/route.ts
+++ src/app/api/running/route.ts
@@ -1,50 +1,65 @@
 import { SaveRunningResultRequestBody } from "@/types/api.types";
 import { NextRequest, NextResponse } from "next/server";
-import { authAdmin, firestoreAdmin } from "../../../../lib/firebase/admin";
-import { cookies } from "next/headers";
+import { firestoreAdmin } from "../../../../lib/firebase/admin";
 import { FieldValue, Timestamp } from "firebase-admin/firestore";
+import { authenticate } from "../../../../lib/firebase/auth-middleware";
+import { chunksArray } from "@/utils/chunks";
 
 export async function POST (request : NextRequest) {
     try{
-        const sessionCookies = (await cookies()).get('session');
-        const idToken = sessionCookies.value;
-        
-        const decodedToken = await authAdmin.verifyIdToken(idToken);
+        const decodedToken = await authenticate();
         const uid = decodedToken.uid;
 
         const body = (await request.json()) as SaveRunningResultRequestBody;
 
-        const {runData , pathPoints } = body;
-        const {runTitle , date , startTime , endTime ,totalDistance, totalRunningTimeSecond , avgPace , centerMapLat, centerMapLng , dayOfWeek} = runData;
+        const {runData , pathPoints} = body;
+        const {runTitle , date , startTime , endTime ,totalDistance, totalRunningTimeSecond , avgPace , dayOfWeek} = runData;
+    
+        const transActionResult = await firestoreAdmin.runTransaction(async(transaction) => {
+            const userRunCountRef = firestoreAdmin.collection('userRunCounter').doc(uid);
+            const runsDocRef = firestoreAdmin.collection('runs').doc();
 
-        const userRunCountRef = firestoreAdmin.collection('userRunCounter').doc(uid);
-        const runsDocRef = firestoreAdmin.collection('runs')
+            const runCountDoc = await transaction.get(userRunCountRef);
+            const currentRunCount = runCountDoc.exists ? runCountDoc.data()?.runCount : 0;
+            const newRunCount = currentRunCount + 1;
 
-        //여기서 runCouterRef로 runcount 갖고오기
-        const runCountDoc = await userRunCountRef.get();
-        const userRunCount = runCountDoc.data().runCount || 0;
-        const updateRunCount = userRunCount + 1;
-        //여기서 runDoc 생성
-        await runsDocRef.add(({
-            runTitle, 
-            avgPace,
-            date,
-            dayOfWeek,
-            endTime,
-            startTime,
-            totalDistance,
-            totalRunningTimeSecond,
-            uid,
-            userRunCount : updateRunCount,
-            createdAt : Timestamp.now()
-        }))
+            transaction.set(runsDocRef , {
+                runTitle, 
+                avgPace,
+                date,
+                dayOfWeek,
+                endTime,
+                startTime,
+                totalDistance,
+                totalRunningTimeSecond,
+                uid,
+                userRunCount : newRunCount,
+                createdAt : Timestamp.now()
+            }); //runsDoc 에 저장
 
-        await userRunCountRef.update({
-            runCount: FieldValue.increment(1) // 기존 값에 1을 더함
-        });
-
-
-        return NextResponse.json({ message: '달리기 결과가 성공적으로 저장되었습니다.', runCount: updateRunCount }, { status: 200 });
+            if(runCountDoc.exists){
+                transaction.update(userRunCountRef , {
+                    runCount : FieldValue.increment(1)
+                })
+            }else {
+                transaction.set(userRunCountRef , {runCount : 1}) // 이전 기록이 없으면 1로 저장
+            }
+            return {runsDocRef , newRunCount}; // 업데이트 된 runCount 와 runsDocRef 리턴
+        })
+        
+        if(pathPoints  && pathPoints.length > 0) {
+            const chunkedPoints = chunksArray(20, pathPoints);
+            const chunkSave = chunkedPoints.map(({point , chunkId})=> {
+                const chunkRef = transActionResult.runsDocRef.collection('pathpoints').doc();
+                return chunkRef.set({
+                    chunkId : chunkId,
+                    pathPoints : point
+                })
+            })
+            await Promise.all(chunkSave);
+        }
+        
+        return NextResponse.json({ message: '달리기 결과가 성공적으로 저장되었습니다.', runCount: transActionResult.newRunCount }, { status: 200 });
     }catch(error){
         console.error(error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
