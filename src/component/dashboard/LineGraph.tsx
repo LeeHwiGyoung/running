e2e616b27d43518dae93c8efb808834e1b015300
HwiGyoung Lee <ter0000@naver.com>
2025-08-04 17:38:26 +0900

    🎨 LineGraph : 데이터 구조 변경으로 인한 코드 구조 변경

--- src/component/dashboard/LineGraph.tsx
+++ src/component/dashboard/LineGraph.tsx
@@ -3,33 +3,10 @@ import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3';
 import {  formatTime } from '@/utils/format';
 
-interface Geometry {
-  type: string;
-  coordinates: number[]; // [경도, 위도, 고도(선택 사항)]
-}
 
-// 개별 지점의 속성 데이터 타입
-interface Properties {
-  id: number;
-  timestamp: string; // ISO 8601 형식의 날짜/시간 문자열
-  elapsedTimeSeconds: number; // 경과 시간 (초)
-  distanceFromPrevKm: number; // 이전 지점으로부터의 거리 (km)
-  cumulativeDistanceKm: number; // 누적 거리 (km)
-  instantPaceSecondsPerKm: number; // 순간 페이스 (초/km)
-  instantPaceFormatted: string; // 순간 페이스 (분:초 형식)
-  averagePaceSecondsPerKm: number; // 평균 페이스 (초/km)
-  averagePaceFormatted: string; // 평균 페이스 (분:초 형식)
-}
-
-// 전체 Feature 객체 타입
-interface PaceFeature {
-  geometry: Geometry;
-  properties: Properties;
-}
-
-interface LineGraphProps {
+interface LineGraphProps<T> {
     id: string;
-    data :  PaceFeature[];
+    data : T[];
     xKey : string;
     yKey : string;
     width: number;
@@ -47,24 +24,23 @@ interface LineGraphProps {
     setButtonText : React.Dispatch<React.SetStateAction<string>>;
     setHoveredSegmentId : React.Dispatch<React.SetStateAction<number|null>>
 }
-export default function LineGraph({data , id , xKey , yKey ,reverseYAxis= false , xAxisFontSize= 16, yAxisFontSize = 16, lineColor = '#B0C4DE', lineWidth = 3, width =400 ,height=200, marginBottom= 20, marginLeft=20, marginRight=20 , marginTop= 20 , setButtonText , setHoveredSegmentId, tickFormatFunc}:LineGraphProps) {
+export default function LineGraph<T extends { timestamp: number }>({data , id , xKey , yKey ,reverseYAxis= false , xAxisFontSize= 16, yAxisFontSize = 16, lineColor = '#B0C4DE', lineWidth = 3, width =400 ,height=200, marginBottom= 20, marginLeft=20, marginRight=20 , marginTop= 20 , setButtonText , setHoveredSegmentId, tickFormatFunc}:LineGraphProps<T>) {
   const svgRef = useRef<SVGSVGElement | null>(null);
+  const startTime = data[0].timestamp;
   
-  useEffect(()=> {
-    const processedData = data.slice(1); 
+  useEffect(()=> { 
     const svg = d3
     .select(svgRef.current)
     .attr('width' , width) //width 설정
     .attr('height', height) //height 설정
     .attr('viewBox', [0,0, width, height])
-    .attr('style', 'max-width : 100%; height : auto')
-
+    .attr('style', 'max-width : 100%; height : auto');
+    
     svg.selectAll('*').remove(); //svg 초기화
+    const xMax = d3.max(data, d => d[xKey]) || 0;
 
-    const xMax = d3.max(processedData, d => d.properties[xKey]) || 0;
-
-    const yMin = d3.min(processedData, d => d.properties[yKey]) || 0;
-    const yMax = d3.max(processedData, d => d.properties[yKey]) || 0;
+    const yMin = d3.min(data, d => d[yKey]) || 0;
+    const yMax = d3.max(data, d => d[yKey]) || 0;
 
     const x = d3
     .scaleLinear()
@@ -82,9 +58,10 @@ export default function LineGraph({data , id , xKey , yKey ,reverseYAxis= false 
     y.range([height - marginBottom , marginTop])
 
 
-    const line = d3.line<PaceFeature>()
-      .x(d => x(d.properties[xKey])) // X 좌표
-      .y(d => y(d.properties[yKey])) // Y 좌표
+    const line = d3.line()
+      .x(d => x(d[xKey])) // X 좌표
+      .y(d => y(d[yKey])) // Y 좌표
+      .defined(d => d.curPace !== null)
       .curve(d3.curveBasis);
 
     svg //x축 생성
@@ -120,7 +97,7 @@ export default function LineGraph({data , id , xKey , yKey ,reverseYAxis= false 
     
     svg //line 그래프 생성
     .append('path')
-    .datum(processedData)
+    .datum(data)
     .attr("fill", "none")
     .attr("stroke", `${lineColor}`)
     .attr("stroke-width", `${lineWidth}`)
@@ -161,36 +138,36 @@ export default function LineGraph({data , id , xKey , yKey ,reverseYAxis= false 
 
     function mousemove(event : MouseEvent) {
       const mouseX = x.invert(d3.pointer(event)[0]); //마우스 x 좌표를 데이터 x값으로 변환
-      const bisect = d3.bisector((d : PaceFeature) => d.properties.cumulativeDistanceKm).left;
-      const i = bisect(processedData, mouseX , 1)
-      const d0 = processedData[i - 1];
-      const d1 = processedData[i];
-      let d : PaceFeature;
+      const bisect = d3.bisector((d) => d[xKey]).left;
+      const i = bisect(data, mouseX , 1)
+      const d0 = data[i - 1];
+      const d1 = data[i];
+      let d ;
 
       if (!d1) { // d1이 undefined (즉, x0가 모든 데이터보다 크거나 같을 때)
         d = d0; // d0가 마지막 데이터이므로 d0를 선택
       } else if (!d0) { // d0가 undefined (즉, x0가 첫 데이터보다 작을 때, i=0인 경우)
         d = d1; // d1이 첫 데이터이므로 d1을 선택 (사실 processedData.slice(1)을 쓰므로 이 경우는 발생 가능성 낮음)
       } else { // d0와 d1이 모두 유효할 때, 더 가까운 데이터 선택
-        d = mouseX - d0.properties[xKey] > d1.properties[xKey] - mouseX ? d1 : d0;
+        d = mouseX - d0[xKey] > d1[xKey] - mouseX ? d1 : d0;
       }
       
-      const tooltipX = x(d.properties[xKey]);
+      const tooltipX = x(d[xKey]);
       focus.attr("transform", `translate(${tooltipX})`)
       focus.select('.x-hover-text')
        .attr('x', 0) // focus 그룹 기준 X=0 (즉, 수직선 바로 위)
        .attr('y', 15)
-       .text(`${formatTime(d.properties.elapsedTimeSeconds)}`)
+       .text(`${formatTime((d.timestamp - startTime))}`)
 
        if(tickFormatFunc){
-         setButtonText(tickFormatFunc(d.properties[yKey]))
+         setButtonText(tickFormatFunc(d[yKey]))
        }else{
-         setButtonText(d.properties[yKey])
+         setButtonText(d[yKey])
        }
-       setHoveredSegmentId(d.properties.id)
+       setHoveredSegmentId(d.id)
 
     }
-  }, [])
+  }, [ ])
 
   return (
     <svg id={id} ref={svgRef}></svg>
