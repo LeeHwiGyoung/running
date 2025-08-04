885b87aa48499afbf452f03cb79d50be921520cd
HwiGyoung Lee <ter0000@naver.com>
2025-08-04 17:37:01 +0900

    ✨ dashboard/runningDetail/[id]/page.tsx : 서버사이드에서 id가 일치하는 러닝 정보를 fetch 하여 runningDetail 페이지 렌더링

--- src/app/dashboard/runningDetail/[id]/page.tsx
+++ src/app/dashboard/runningDetail/[id]/page.tsx
@@ -3,36 +3,37 @@ import BackButton from '@/component/BackButton'
 import React from 'react'
 import Header from '@/component/Header'
 import DetailRunPerformaceView from '@/component/dashboard/DetailRunPerformaceView'
+import { headers } from 'next/headers'
+import { PathPoint, RunningData } from '@/types/running.types'
 
-const dummyDetail = {
-    id: 0,
-    date: '2025-06-09',
-    title : "월요일 저녁 러닝",
-    distance : 3,
-    distanceLabel : 'Km',
-    runningPace : 450,
-    runningTime : 1200,
-    cadence : 180,
-    altitude : 2,
-}
+export default async function page({params} : {params :  {id : string}}) {
+  const id = params.id;
+  const res = await fetch(`http://localhost:3000/api/dashboard/runningDetail/${id}` , {
+      credentials: 'include',
+      cache: 'no-store',
+      headers: {
+        Cookie: (await headers()).get('cookie') || '',
+      }
+  });
+  const runDetailData  = await res.json();
+  const runDetail = runDetailData.runDetail as RunningData;
+  const pathPoints = runDetailData.pathPoints as PathPoint[];
 
-export default function page() {  
   return (
     <main>
       <Header className='w-full mb-4' borderBottomShadow={true}>
         <BackButton />
       </Header>
-      <DetailRunCard 
-        date={dummyDetail.date}
-        title={dummyDetail.title}
-        distance={dummyDetail.distance}
-        distanceLabel={dummyDetail.distanceLabel}
-        runningPace={dummyDetail.runningPace}
-        runningTime={dummyDetail.runningTime}
-        cadence={dummyDetail.cadence} 
+      <DetailRunCard
+        date={runDetail.date}
+        title={runDetail.runTitle}
+        distance={runDetail.totalDistance}
+        distanceLabel={'Km'}
+        runningPace={runDetail.avgPace}
+        runningTime={runDetail.totalRunningTimeSecond}
       />
       <section className='mb-18'>
-        <DetailRunPerformaceView position={{latitude : 37.4504924 , longitude : 126.6849254}}/>
+        <DetailRunPerformaceView position={{latitude : 37.4504924 , longitude : 126.6849254}} pathPoints={pathPoints}/>
       </section>
     </main>
   )
