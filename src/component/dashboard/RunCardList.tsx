b75e3ebe05121c62b95bea393e85eb5fc74263fd
HwiGyoung Lee <ter0000@naver.com>
2025-08-04 17:39:23 +0900

    🎨 RunCardList  :  더미데이터 제거 및 해당 유저의 러닝 데이터를 fetch 하도록 변경

--- src/component/dashboard/RunCardList.tsx
+++ src/component/dashboard/RunCardList.tsx
@@ -1,57 +1,44 @@
 'use client';
 import { usePathname, useRouter } from "next/navigation";
 import RunCard from "./RunCard";
+import { useEffect, useState } from "react";
+import { RunningData } from "@/types/running.types";
 
-const RunCardDummyData = 
-    [ 
-        {
-            id: 0,
-            date: '2025-06-09',
-            title : "월요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200,
-        }, 
-        {
-            id: 1,
-            date: '2025-06-08',
-            title : "일요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200, 
-        },
-        {
-            id: 2,
-            date: '2025-06-07',
-            title : "토요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200, 
-        },
-        {
-            id: 3,
-            date: '2025-06-06',
-            title : "금요일 저녁 러닝",
-            distance : 3,
-            distanceLabel : 'Km',
-            runningPace : 450,
-            runningTime : 1200, 
-}] 
 export default function RunCardList() {
   const router = useRouter();
-  const pathname = usePathname() 
+  const pathname = usePathname()
+  const [runData , setRunData] = useState<RunningData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  
   const onClickRunCard = (id : number | string) => {
     router.push(`${pathname}/runningDetail/${id}`)
   }
 
+  useEffect(() => {
+    const getRunningCardList = async () => {
+        setIsLoading(true);
+        try{
+            const res = await fetch('api/dashboard/runCardList');
+            if(!res.ok){
+                throw new Error('failed fetch runCardList')
+            }
+            const data = await res.json();
+            setRunData(data.runs);
+        }catch(error){
+            console.error('Error fetching runs:', error);
+            // 사용자에게 오류 메시지를 표시하는 등의 에러 처리 로직 추가
+        }finally{
+            setIsLoading(false);
+        }
+    } 
+    getRunningCardList()
+  }, [])
+
   return (
     <ul className='flex flex-col gap-8 px-2'>
-        {RunCardDummyData.map((runData)=> 
+        {runData.map((runData)=> 
             <li key={runData.id}>
-                <RunCard onClick={() => onClickRunCard(runData.id)} date={runData.date} title={runData.title} distance={runData.distance} distanceLabel={runData.distanceLabel} runningPace={runData.runningPace} runningTime={runData.runningTime}/>
+                <RunCard onClick={() => onClickRunCard(runData.id)} date={runData.date} title={runData.runTitle} distance={runData.totalDistance} distanceLabel={'Km'} runningPace={runData.avgPace} runningTime={runData.totalRunningTimeSecond}/>
             </li>
         )}     
     </ul>
