567961f2aefa5f73a14b1a77368f212f03e9adba
HwiGyoung Lee <ter0000@naver.com>
2025-08-04 17:34:57 +0900

    ✨ api/dashboard/runningDetail/[id] : id 값에 일치하는 러닝 정보를 가지고 오는 API 추가

---
+++ src/app/api/dashboard/runningDetail/[id]/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { authenticate } from '../../../../../../lib/firebase/auth-middleware';
+import { firestoreAdmin } from '../../../../../../lib/firebase/admin';
+export async function GET(request:NextRequest , {params} : {params : { id : string}}) {
+    try {
+        const decodedToken = await authenticate(); //로그인 인증 및 decodedToken 반환
+        const id = params.id;
+        const uid = decodedToken.uid;
+
+        console.log(id, 'id')
+        const runDocsRef = firestoreAdmin.collection('runs').doc(id);
+
+        const runSnapShot = await runDocsRef.get();
+
+        if(!runSnapShot.exists) {
+            console.error('달리기 기록이 없습니다.')
+            return  NextResponse.json({ runDetail: []  , status : 200})
+        }
+
+        if (runSnapShot.data().uid !== uid) {
+            return NextResponse.json({ error: 'Unauthorized', status: 403 });
+        }
+
+        const runDetailData = runSnapShot.data();
+
+        const pathPointsRef = firestoreAdmin.collection('runs').doc(id).collection('pathpoints');
+        const pathPointsSnapShot = await pathPointsRef.get();
+
+        const pathPointChunks = pathPointsSnapShot.docs.map(doc => doc.data());
+        pathPointChunks.sort((a, b) => a.chunkId - b.chunkId);
+
+        const totalPathPoints = [];
+        
+        pathPointChunks.map((chunk) => {
+            if(chunk.pathPoints && Array.isArray(chunk.pathPoints)){
+                chunk.pathPoints.sort((a, b) => a.timestamp - b.timestamp);
+                totalPathPoints.push(...chunk.pathPoints)
+            }
+        })
+    
+        return NextResponse.json({runDetail : runDetailData , pathPoints : totalPathPoints , status : 200})
+    }
+    catch(error){
+        console.error('런 기록 조회 중 오류 발생:', error);
+        return NextResponse.json({error :  error.message || '서버 오류가 발생했습니다' , status : 500});       
+    }
+}
