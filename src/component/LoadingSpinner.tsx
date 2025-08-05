b1f6abdd37525b09c87105997ead5d9f5c72101c
HwiGyoung Lee <ter0000@naver.com>
2025-08-05 15:19:17 +0900

    ✨ LoadingSpinner.tsx : 로딩 스피너 추가

---
+++ src/component/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LoadingSpinner() {
+  return (
+      <div className="absolute  top-[50%] left-[50%] translate-[-50%] animate-spin rounded-full h-8 w-8 border-4 border-gray-300 border-t-blue-600 "></div>  
+  )
+}
