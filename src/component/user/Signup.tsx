7e12d763e26e6eae856d097cd7cb1d78bd01973c
HwiGyoung Lee <ter0000@naver.com>
2025-08-05 15:21:46 +0900

    ✨ Signup.tsx : 회원가입 API 요청시 로딩 스피너 추가하여 UX 향상

--- src/component/user/Signup.tsx
+++ src/component/user/Signup.tsx
@@ -4,6 +4,7 @@ import AuthInput from './AuthInput'
 import Button from '../Button';
 import { useSignup } from '@/hooks/useSignup';
 import AuthHeader from './AuthHeader';
+import LoadingSpinner from '../LoadingSpinner';
 
 export default function Signup() {
   const [email , setEmail] = useState<string>("");
@@ -32,24 +33,18 @@ export default function Signup() {
     signup({email, password , nickname});
   }
   
-  if(isLoading) {
-    return (
-        <div>Loading...</div>
-    )
-  }
-
   
-
   return (
     <article className='relative h-[calc(100dvh-4rem)]'>
         <AuthHeader>
             <h1>회원가입</h1>
         </AuthHeader>
         <form 
-         className='px-4'
+         className='flex flex-col gap-4 items-center px-4'
          onSubmit={handleSubmit}
         >
           <AuthInput
+           className='w-full'
            value={email}
            label={'이메일'}
            type={'email'}
@@ -59,6 +54,7 @@ export default function Signup() {
           />
 
           <AuthInput
+           className='w-full'
            value={password}
            label={'패스워드'}
            type={'password'}
@@ -68,6 +64,7 @@ export default function Signup() {
            />
 
           <AuthInput
+           className='w-full'
            value={nickname}
            label={'닉네임'}
            type={'text'} 
@@ -76,10 +73,13 @@ export default function Signup() {
            onChange={hanldleChangeNickname}
           />
 
-          <Button className="absolute bottom-0 text-2xl bg-black text-white rounded-full w-full py-2 " type='submit'>
+          <Button className="text-2xl bg-black text-white rounded-full w-full py-2 mt-4" type='submit'>
             회원가입
           </Button>
          </form>
+        {isLoading &&  <div className='fixed top-0 left-0 bg-black/50 w-full h-[100vh] z-10000'>
+          <LoadingSpinner/>
+        </div>}
     </article>
   )
 }
