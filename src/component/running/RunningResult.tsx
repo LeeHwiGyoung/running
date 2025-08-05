f0285496bb6cca95ea67e5a36e6a6c9234efdf63
HwiGyoung Lee <ter0000@naver.com>
2025-08-05 16:51:25 +0900

    ✨ RunningResult : 현재 날짜와 시간을 기준으로 제목을 자동으로 생성해주는 기능 추가

--- src/component/running/RunningResult.tsx
+++ src/component/running/RunningResult.tsx
@@ -7,17 +7,19 @@ import Button from '../Button';
 import { useRunningStore } from '@/store/useRunningStore';
 import { SaveRunningResultRequestBody } from '@/types/api.types';
 import { RunningSummaryForSave } from '@/types/running.types';
-import { formatTimestampToDayOfWeek, formatTimestampToYYYYMMDD } from '@/utils/format';
+import { formatDayOfWeek, formatTimestampToYYYYMMDD } from '@/utils/format';
+import { getTimeOfDay, getToday } from '@/utils/getToday';
 
 export default function RunningResult() {
-  const [title , setTitle] = useState<string>("화요일 오후 러닝")
-  
+  const {today , day} = getToday();
+
+  const [title , setTitle] = useState<string>(`${formatDayOfWeek(day)} ${getTimeOfDay()} 러닝`)
   const {runningStartTime , runningEndTime , totalDistance, totalTime ,averagePace, path } = useRunningStore()
   
   const runData : RunningSummaryForSave = {
     runTitle : title,
-    date : formatTimestampToYYYYMMDD(Date.now()),
-    dayOfWeek : formatTimestampToDayOfWeek(Date.now()),
+    date : formatTimestampToYYYYMMDD(day),
+    dayOfWeek : formatDayOfWeek(Date.now()),
     startTime : runningStartTime,
     endTime : runningEndTime,
     totalDistance : totalDistance,
@@ -47,13 +49,13 @@ export default function RunningResult() {
 
   return (
     <article className='mb-6'>
-        <Header>
+        <Header className='flex items-center'>
             <h1>러닝 결과</h1>
-            <Button onClick={handleSaveRunning}>
+            <Button className='border rounded-full px-4 py-2 bg-black text-white text-sm ml-auto' onClick={handleSaveRunning}>
               저장
             </Button>
         </Header>
-        <DetailRunCard isEditable={true} date={'2025-06-24'} title={title} distance={3} distanceLabel={'km'} runningPace={0} runningTime={0}  handleChangeTitle={handleChangeTitle}/>
+        <DetailRunCard isEditable={true} date={today} title={title} distance={3} distanceLabel={'km'} runningPace={0} runningTime={0}  handleChangeTitle={handleChangeTitle}/>
         <KakaoMap className='m-auto w-[calc(100%-2rem)] h-[50dvh] mt-4' mapId={'resultMap'} latitude={37.4504924} longitude={126.6849254} level={3} />
     </article>
   )
