fa8b41535f323eb3593a6715495976d2598aa43b
HwiGyoung Lee <ter0000@naver.com>
2025-08-05 15:19:52 +0900

    🎨 RunCardList.tsx : 데이터 페칭 로직을 서버컴포넌트로 분리

--- src/component/dashboard/RunCardList.tsx
+++ src/component/dashboard/RunCardList.tsx
@@ -1,46 +1,26 @@
 'use client';
 import { usePathname, useRouter } from "next/navigation";
 import RunCard from "./RunCard";
-import { useEffect, useState } from "react";
 import { RunningData } from "@/types/running.types";
 
-export default function RunCardList() {
+interface RunCardListProps {
+  runData : RunningData[];
+}
+export default function RunCardList({runData} : RunCardListProps) {
   const router = useRouter();
   const pathname = usePathname()
-  const [runData , setRunData] = useState<RunningData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   
   const onClickRunCard = (id : number | string) => {
     router.push(`${pathname}/runningDetail/${id}`)
   }
 
-  useEffect(() => {
-    const getRunningCardList = async () => {
-        setIsLoading(true);
-        try{
-            const res = await fetch('api/dashboard/runCardList');
-            if(!res.ok){
-                throw new Error('failed fetch runCardList')
-            }
-            const data = await res.json();
-            setRunData(data.runs);
-        }catch(error){
-            console.error('Error fetching runs:', error);
-            // 사용자에게 오류 메시지를 표시하는 등의 에러 처리 로직 추가
-        }finally{
-            setIsLoading(false);
-        }
-    } 
-    getRunningCardList()
-  }, [])
-
   return (
-    <ul className='flex flex-col gap-8 px-2'>
-        {runData.map((runData)=> 
-            <li key={runData.id}>
-                <RunCard onClick={() => onClickRunCard(runData.id)} date={runData.date} title={runData.runTitle} distance={runData.totalDistance} distanceLabel={'Km'} runningPace={runData.avgPace} runningTime={runData.totalRunningTimeSecond}/>
-            </li>
-        )}     
-    </ul>
+      <ul className='flex flex-col gap-8 px-2'>
+          {runData.map((runData)=> 
+              <li key={runData.id}>
+                  <RunCard onClick={() => onClickRunCard(runData.id)} date={runData.date} title={runData.runTitle} distance={runData.totalDistance} distanceLabel={'Km'} runningPace={runData.avgPace} runningTime={runData.totalRunningTimeSecond}/>
+              </li>
+          )}     
+      </ul>
   )
 }
