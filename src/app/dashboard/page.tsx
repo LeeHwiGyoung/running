6bb130be6050dfaf4f30593d6188b3636842be9a
HwiGyoung Lee <ter0000@naver.com>
2025-08-12 15:58:23 +0900

    ✨ dashboard/page.tsx : searchParams 를 props 로 받아 totalFetcher 로 전달

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,19 @@
-
 import RunCardListFetcher from '@/component/dashboard/RunCardListFetcher'
-import Total from '@/component/dashboard/Total'
+import TotalFetcher from '@/component/dashboard/TotalFetcher'
 import Header from '@/component/Header'
 import AuthHeader from '@/component/user/AuthHeader'
 import React from 'react'
 
-
-export default function page() {
+export default async function page({searchParams}) {
+  const searchParam = await searchParams;
+  const week : 'current' | 'prev' = searchParam.week || 'current';
   return (
     <>
       <AuthHeader>
         <h1>통계</h1>
       </AuthHeader>
       <main className='mb-16'>
-        <Total/>
+        <TotalFetcher week={week}/>
         <section>
           <Header>
             <h3>최근 활동</h3>
