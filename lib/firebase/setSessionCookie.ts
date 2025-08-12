b3ac7ddadb19fe3dd5e76ba035f9086ee47110cd
HwiGyoung Lee <ter0000@naver.com>
2025-08-12 15:56:11 +0900

    🎨 setSessionCookie.ts :firebase와 browser 의 maxage 차이로 인한 수정

--- lib/firebase/setSessionCookie.ts
+++ lib/firebase/setSessionCookie.ts
@@ -6,7 +6,7 @@ export async function setSessionCookie(idToken: string, expiresIn: number) {
     const cookieStore = await cookies();
     // Next.js `cookies()` 함수를 사용하여 쿠키 설정
      cookieStore.set('session', sessionCookie, {
-        maxAge: expiresIn,
+        maxAge: expiresIn / 1000,
         httpOnly: true, // JavaScript 접근 불가
         secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
         sameSite: 'strict',
