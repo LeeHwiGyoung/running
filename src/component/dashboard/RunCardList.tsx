06de8da23ed68b2554663d6b698a45dbfc8cc915
HwiGyoung Lee <ter0000@naver.com>
2025-08-15 16:24:41 +0900

    ✨ RunCardList.tsx: RunCardList에 무한 스크롤 기능 추가
    - IntersectionObserver 커스텀 훅(useScroll)을 사용하여 구현
    - 리스트의 마지막 요소가 뷰포트에 절반 이상 노출되면 다음 데이터 fetch
    - Next.js 서버 컴포넌트에서 받은 초기 데이터와 결합

--- src/component/dashboard/RunCardList.tsx
+++ src/component/dashboard/RunCardList.tsx
@@ -1,26 +1,88 @@
 'use client';
 import { usePathname, useRouter } from "next/navigation";
 import RunCard from "./RunCard";
-import { RunningData } from "@/types/running.types";
+import { NextCursor, RunningData } from "@/types/running.types";
+import { useCallback, useState } from "react";
+import useScroll from "@/hooks/useScroll";
+import LoadingSpinner from "../LoadingSpinner";
 
 interface RunCardListProps {
-  runData : RunningData[];
+  initRunData : RunningData[];
+  initNextCursor : NextCursor;
 }
-export default function RunCardList({runData} : RunCardListProps) {
+
+export default function RunCardList({initRunData ,initNextCursor} : RunCardListProps) {
   const router = useRouter();
   const pathname = usePathname()
-  
+  const [runningData , setRunningData] = useState<RunningData[]>(initRunData);
+  const [cursor, setCursor] = useState<NextCursor>(initNextCursor);
+  const [loading, setLoading] = useState<boolean>(false);
+
   const onClickRunCard = (id : number | string) => {
     router.push(`${pathname}/runningDetail/${id}`)
   }
 
+  const getRunningCardList = async(nextCursor : NextCursor) => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dashboard/runCardList?lastDocId=${nextCursor.lastDocId}&lastCreatedAt=${nextCursor.lastCreatedAt}`,{
+          credentials: 'include',
+      });
+  
+      if (!res.ok) {
+          throw new Error('Failed to fetch runCardList');
+      }
+      
+      const json = await res.json();
+
+      return json;
+  }
+
+  const loadMoreRunData = useCallback(async() => {
+    if(loading || !cursor) return; //로딩중이거나 가져올 데이터가 없다면 return;
+    setLoading(true);
+    try {
+      const {runs : newRunData, nextCursor }= await getRunningCardList(cursor); // 여기서 fetch;
+      
+      if(newRunData.length > 0) { // 추가 데이터가 있다면
+        setRunningData(prevRunningData => [...prevRunningData, ...newRunData]);
+        setCursor(nextCursor);
+      }else {
+        setCursor(null)
+      }
+    }catch(error){ 
+      console.error('failed Fetch RunningData' , error)
+    }finally{
+      setLoading(false)
+    }
+  }, [loading, cursor])
+
+  const setObserverRef = useScroll({
+    onIntersect : (entries) => {
+      //뷰포트에 마지막 요소가 보이면
+      if(entries[0].isIntersecting) {
+        loadMoreRunData()
+      }
+    }, options : {
+      root :null,
+      rootMargin : '0px',
+      threshold : 0.5,
+    }
+  })
+
   return (
       <ul className='flex flex-col gap-8 px-2'>
-          {runData.map((runData)=> 
-              <li key={runData.id}>
+          {runningData.map((runData,index)=> {
+            const isLastRunningData = runningData.length === index + 1;
+            return (
+              <li key={runData.id} ref={isLastRunningData ? setObserverRef : null}>  {/*  콜백 ref 이용 node를 콜백 인수로 받는 콜백 함수 사용 가능 */}
                   <RunCard onClick={() => onClickRunCard(runData.id)} date={runData.date} title={runData.runTitle} distance={runData.totalDistance} distanceLabel={'Km'} runningPace={runData.avgPace} runningTime={runData.totalRunningTimeSecond}/>
               </li>
-          )}     
+            )
+          })}     
+          {loading && (
+            <li className="relative">
+              <LoadingSpinner/>
+            </li>
+          )}
       </ul>
   )
 }
