1e5cefd2097c1d05f05593f0d8473999cf37a5ca
HwiGyoung Lee <ter0000@naver.com>
2025-08-30 17:22:28 +0900

    💄 Total.tsx : 요일별 러닝 기록 호버시 러닝 정보 위치 수정

--- src/component/dashboard/Total.tsx
+++ src/component/dashboard/Total.tsx
@@ -33,12 +33,14 @@ export default function Total({initData} : TotalProps) {
   }
   
   return (
-    <section className='relative px-2'>
+    <section className='px-2'>
       <h2 className='sr-only'>총 러닝 통계</h2>
       <TotalCategory position='right' className='m-2'/>
-      <RunningInfo className='rounded-sm border-gray-100 border shadow-[0px_2px_5px_rgba(0,0,0,0.1)] py-4' runCount={totalData.totalRunCount} distance={totalData.totalDistance} time={totalData.totalTime} />
-      <BarGraph data={runningData} xKey={'day'} yKey={'distance'} width={540} height={200} marginBottom={30} onClickBar={onClickBar} onClickOutside={onClickOutsideBar} xAxisFontSize={16} yAxisFontSize={12} barColor={'#ADD8E6'} hoverBarColor={'#B0C4DE'}/>
-      {selectedBar !== null && <RunningInfo className='absolute top-0 z-index-100 bg-white rounded-sm border-gray-100 border shadow-[0px_2px_5px_rgba(0,0,0,0.1)] py-4' runCount={runningData[selectedBar].runCount} distance={runningData[selectedBar].distance} time={runningData[selectedBar].time}/>}
+      <div className='relative'>
+        <RunningInfo className='rounded-sm border-gray-100 border shadow-[0px_2px_5px_rgba(0,0,0,0.1)] py-4' runCount={totalData.totalRunCount} distance={totalData.totalDistance} time={totalData.totalTime} />
+        <BarGraph data={runningData} xKey={'day'} yKey={'distance'} width={540} height={200} marginBottom={30} onClickBar={onClickBar} onClickOutside={onClickOutsideBar} xAxisFontSize={16} yAxisFontSize={12} barColor={'#ADD8E6'} hoverBarColor={'#B0C4DE'}/>
+        {selectedBar !== null && <RunningInfo className='absolute top-0 z-index-100 bg-white rounded-sm border-gray-100 border shadow-[0px_2px_5px_rgba(0,0,0,0.1)] py-4' runCount={runningData[selectedBar].runCount} distance={runningData[selectedBar].distance} time={runningData[selectedBar].time}/>}
+      </div>
     </section>
   )
 }
