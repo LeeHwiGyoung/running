e7d09e22363e756a6c3fb152ce7f583f79f63c63
HwiGyoung Lee <ter0000@naver.com>
2025-08-30 17:20:37 +0900

    💄 LoadingSpinner.tsx : z-index 추가

--- src/component/LoadingSpinner.tsx
+++ src/component/LoadingSpinner.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function LoadingSpinner() {
   return (
-      <div className="absolute  top-[50%] left-[50%] translate-[-50%] animate-spin rounded-full h-8 w-8 border-4 border-gray-300 border-t-blue-600 "></div>  
+      <div className="absolute top-[50%] left-[50%] translate-[-50%] animate-spin rounded-full h-8 w-8 border-4 border-gray-300 border-t-blue-600 z-[100000]"></div>  
   )
 }
