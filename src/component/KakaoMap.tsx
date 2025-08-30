b6ecbb11dc3b248e58e2305d40eeef5d7df38009
HwiGyoung Lee <ter0000@naver.com>
2025-08-30 17:20:22 +0900

    ✨ KakaoMap.tsx : 러닝 좌표를 바탕으로 polyline을 생성하고 선택된 segment에 따라 폴리라인의 색상이 변경되는 기능 추가

--- src/component/KakaoMap.tsx
+++ src/component/KakaoMap.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Coordinate } from '@/types/type';
 import Script from 'next/script';
 import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from 'react';
 
@@ -23,12 +24,14 @@ interface KakaoMapProps {
     longitude: number;
   }[],
   hoveredSegmentId ?: number|null;
+  points ?: Coordinate[];
 }
 
-const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className="",mapId , level , latitude , longitude }, ref) => {
+const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className="",mapId , level , latitude , longitude , points , hoveredSegmentId }, ref) => {
   const map = useRef(null);
   const mapRef = useRef<HTMLDivElement>(null);
-     
+  
+  const polyline = useRef(null);
   useImperativeHandle(ref, () => ({
     getMap: () => map.current, // 외부에서 지도를 얻을 수 있는 메서드 제공
     getKaKaoInstance : () => window.kakao,
@@ -36,6 +39,7 @@ const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className="",mapId ,
 
   const initializeMap = useCallback(()=> {
      if(!window.kakao.maps || !mapRef.current) return;
+     
      window.kakao.maps.load(()=> {
         const options = {
             center : new window.kakao.maps.LatLng(latitude, longitude),
@@ -45,33 +49,61 @@ const KakaoMap = forwardRef<KakaoMapHandle,KakaoMapProps>(({className="",mapId ,
             disableDoubleClick : true,
             disableDoubleClickZoom  : true,
         }
+        
         map.current = new window.kakao.maps.Map(mapRef.current, options)
-        
-        
+        if(points && points.length > 0) {  
+          const pathPoints = points.map((point) => new window.kakao.maps.LatLng(point.latitude , point.longitude));
+          
+          const bounds = new window.kakao.maps.LatLngBounds();
+          pathPoints.forEach((point) => {
+            bounds.extend(point);
+          })
+          
+          map.current.setBounds(bounds);
+          const polylines = []
+          for(let i = 0 ; i < pathPoints.length - 1 ; i++){
+            const paths = pathPoints.slice(i , i + 2);
+            polylines.push(new window.kakao.maps.Polyline({
+              path : paths,
+              strokeWeight: 4, // 선의 두께 입니다
+              strokeColor: '#000', // 선의 색깔입니다
+              strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+              strokeStyle: 'solid'
+            }))
+          }
+          
+          polyline.current = polylines
+          polyline.current.forEach((line) => line.setMap(map.current))
+
+        }
      })
-  }, [latitude, level, longitude])
+  }, [latitude, level, longitude , points])
 
-
-   /* useEffect(() => {
-    if (hoveredSegmentId && statePolyline) {
-    statePolyline.forEach(({polyline, segmentId }) => {
-      if (segmentId === hoveredSegmentId) { // 조건이 더 단순해졌습니다.
-        polyline.setOptions({
-          strokeColor : '#000000'
-        });
-      }
-    });
-  } else {
-    statePolyline.forEach(({ polyline }) => {
-      polyline.setOptions({
-        strokeWeight: 2,
-        strokeColor: '#FF00FF',
-        strokeOpacity: 0.8
-      });
-    });
+   useEffect(() => {
+    if(!hoveredSegmentId && points && polyline.current) {
+      polyline.current.forEach((line) => {
+        line.setOptions({
+          strokeColor : '#000'
+        })
+      })
+    }
+    if (hoveredSegmentId && points && polyline.current) {
+      polyline.current.forEach((line ,index) => {
+        if(index === hoveredSegmentId - 1){
+          line.setOptions({
+            strokeColor : '#B0C4DE'
+          })
+        }else {
+          line.setOptions({
+            strokeColor : "#000"
+          })
+        }
+      })
+    }
   }
-  }, [hoveredSegmentId, statePolyline]);
- */
+
+  , [hoveredSegmentId, points]);
+ 
    useEffect(() => {
     // 이미 kakao API가 로드되어 있는 경우 바로 초기화
     if (window.kakao?.maps) {
